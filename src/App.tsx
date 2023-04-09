bf6b1d662e2c35dfa61798f05f9339d4eae53951
NickWhit1 <nwhabibulen@yahoo.com>
2023-04-09 01:11:10 -0500

    Styling Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import ListGroup from "./components/ListGroup";
+import "./App.css";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  let items = ["STL", "New York", "Denver"];
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <Alert>"Check it out"</Alert>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectedItem}
+      />
+      {/* <Alert>"Check it out"</Alert>
       <Button color="danger" onClick={() => console.log("Clicked")}>
         My Button
-      </Button>
+      </Button> */}
     </div>
   );
 }
