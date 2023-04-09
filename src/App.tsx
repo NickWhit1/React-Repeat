e2d84566126459031bf7de88773f7d7810bf0968
NickWhit1 <nwhabibulen@yahoo.com>
2023-04-08 23:36:38 -0500

    Added Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
   return (
     <div>
+      <Alert>"Check it out"</Alert>
       <Button color="danger" onClick={() => console.log("Clicked")}>
         My Button
       </Button>
